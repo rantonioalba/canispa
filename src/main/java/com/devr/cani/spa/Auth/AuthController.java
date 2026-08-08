ebbda3172377efdee0a57409cb5995367b4125fa
Roberto Alba <roberto.alba01@grupobimbo.com>
2026-08-08 12:59:13 -0600

    [UPDATE] AuthController using ApiResponse

--- src/main/java/com/devr/cani/spa/Auth/AuthController.java
+++ src/main/java/com/devr/cani/spa/Auth/AuthController.java
@@ -9,22 +9,27 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.devr.cani.spa.controller.base.BaseController;
+import com.devr.cani.spa.dto.response.ApiResponse;
+
 @RestController
 @RequestMapping("/auth")
 @RequiredArgsConstructor
 @CrossOrigin(origins = "http://localhost:4200")
-public class AuthController {
+public class AuthController extends BaseController {
 
     private final AuthService authService;
 
 
     @PostMapping(value = "login")
-    public ResponseEntity<LoginResponse> login(@RequestBody LoginRequest request){
-        return ResponseEntity.ok(authService.login(request));
+    public ResponseEntity<ApiResponse<LoginResponse>> login(@RequestBody LoginRequest request){
+        LoginResponse loginResponse = authService.login(request);
+        return responseSuccess("Login successful", loginResponse);
     }
 
     @PostMapping(value = "register")
-    public ResponseEntity<AuthResponse> register(@RequestBody RegisterRequest request){
-        return ResponseEntity.ok(authService.register(request));
+    public ResponseEntity<ApiResponse<AuthResponse>> register(@RequestBody RegisterRequest request){
+        AuthResponse authResponse = authService.register(request);
+        return responseSuccess("Registration successful", authResponse);
     }
 }
